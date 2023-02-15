51fc065efd47602fb62dfb29ed9bcd7fbc4d3c92
uderk <venislav.zdravkov@gmail.com>
2023-02-15 18:04:34 +0200

    added data set from json file inside the project. Some commented code wa removed.

--- tests/ClientAppPOM.spec.js
+++ tests/ClientAppPOM.spec.js
@@ -1,31 +1,28 @@
 const { test, expect } = require("@playwright/test");
 const {POManager} = require('../PageObjects/POManager')
+const dataSet = JSON.parse(JSON.stringify(require("../tests/utils/placeOrderTestData.json")));
 
-test("Test client app - with POM", async ({page})=>{
-    //define context and page
-    const poManager = new POManager(page);
-    const loginPage = await poManager.getLoginPage();
-    const dashboard = await poManager.getDashboardPage();
-    const orderPage = await poManager.getOrderPage();
-    const ordersHistoryPage = await poManager.getOrdersHistoryPage();
-    //define consts used in scripts 
+for(const data of dataSet){
 
-    const email = "veni.zdravkov@gmail.com";
-    const password = "123456Ve@";
-    const product = "zara coat 3"
-   //actions
-
-    await loginPage.goTo()
-    await loginPage.validLogin(email,password);
-
-    await dashboard.searchProductAndAddToChart(product);
-    await dashboard.navigateToCart();
-    await orderPage.CheckIfProductIsInChart(product); 
-    await dashboard.fillOrderInformation();
-    await dashboard.placeAnOrderAndCheckEmail(email);
-    await dashboard.checkIfOrderIsSuccessful();
-    const orderID = await dashboard.getOrderID();
-    await dashboard.goToOrdersHistoryPage();
-    await ordersHistoryPage.checkOrder(orderID);
-
-});
+ test(`Test client app - with POM ${data.product}`, async ({page})=>{
+     //define context and page
+     const poManager = new POManager(page);
+     const loginPage = await poManager.getLoginPage();
+     const dashboard = await poManager.getDashboardPage();
+     const orderPage = await poManager.getOrderPage();
+     const ordersHistoryPage = await poManager.getOrdersHistoryPage();
+     //actions 
+     await loginPage.goTo()
+     await loginPage.validLogin(data.email,data.password);
+     await dashboard.searchProductAndAddToChart(data.product);
+     await dashboard.navigateToCart();
+     await orderPage.CheckIfProductIsInChart(data.product); 
+     await dashboard.fillOrderInformation();
+     await dashboard.placeAnOrderAndCheckEmail(data.email);
+     await dashboard.checkIfOrderIsSuccessful();
+     const orderID = await dashboard.getOrderID();
+     await dashboard.goToOrdersHistoryPage();
+     await ordersHistoryPage.checkOrder(orderID);
+ 
+ })
+};
